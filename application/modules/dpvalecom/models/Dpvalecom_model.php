cf3d44bc4911f0ce573ce714ef39969db3331694
Luz Alcantar <luz.alcantar@dportenis.com.mx>
2022-09-23 17:36:26 -0600

    fix(TI-DEV/R22)Cambio de servicios a nuevo ACE

--- application/modules/dpvalecom/models/Dpvalecom_model.php
+++ application/modules/dpvalecom/models/Dpvalecom_model.php
@@ -100,8 +100,8 @@ class Dpvalecom_model extends CI_Model {
        $sms_password = $this->config->item('sms_pass');*/
        if(ENVIRONMENT=="development"){
          //NEW BUS
-        $sms_url = "http://10.200.5.89:7085/sms/api/EnviarSMS";
-        // $sms_url = "http://aceqa.grupodp.com.mx:7085/sms/api/EnviarSMS";
+        // $sms_url = "http://10.200.5.89:7085/sms/api/EnviarSMS";
+        $sms_url = "http://aceqa.grupodp.com.mx:7085/sms/api/EnviarSMS";
         //OLD BUS
         // $sms_url = "http://10.200.3.102:7082/sms/api/EnviarSMS";
        }else{
@@ -209,8 +209,8 @@ class Dpvalecom_model extends CI_Model {
        $sms_password = $this->config->item('sms_pass');*/
        if(ENVIRONMENT=="development"){
          //NEW BUS
-        $sms_url = "http://10.200.5.89:7085/sms/api/EnviarSMS";
-        // $sms_url = "http://aceqa.grupodp.com.mx:7085/sms/api/EnviarSMS";
+        // $sms_url = "http://10.200.5.89:7085/sms/api/EnviarSMS";
+        $sms_url = "http://aceqa.grupodp.com.mx:7085/sms/api/EnviarSMS";
         //OLD BUS
         // $sms_url = "http://10.200.3.102:7082/sms/api/EnviarSMS";
        }else{
