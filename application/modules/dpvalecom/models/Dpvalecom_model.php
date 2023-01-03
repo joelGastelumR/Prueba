6d3ab22ba029f8cf695f1c26e9bc8e1eb503f7df
Luz Alcantar <luz.alcantar@dportenis.com.mx>
2023-01-03 14:17:55 -0700

    fix(TI-DEV/R22)Cambio de puerto ACE(7085)

--- application/modules/dpvalecom/models/Dpvalecom_model.php
+++ application/modules/dpvalecom/models/Dpvalecom_model.php
@@ -102,7 +102,7 @@ class Dpvalecom_model extends CI_Model {
        if(ENVIRONMENT=="development"){
          //NEW BUS
         // $sms_url = "http://10.200.5.89:7085/sms/api/EnviarSMS";
-        $sms_url = "http://aceqa.grupodp.com.mx:7082/sms/api/EnviarSMS";
+        $sms_url = "http://aceqa.grupodp.com.mx:7085/sms/api/EnviarSMS";
 
         //OLD BUS
         // $sms_url = "http://10.200.3.102:7082/sms/api/EnviarSMS";
@@ -214,7 +214,7 @@ class Dpvalecom_model extends CI_Model {
         // $sms_url = "http://10.200.5.89:7085/sms/api/EnviarSMS";
         //$sms_url = "http://aceqa.grupodp.com.mx:7083/sms/api/EnviarSMS";
 
-        $sms_url = "http://aceqa.grupodp.com.mx:7082/sms/api/EnviarSMS";
+        $sms_url = "http://aceqa.grupodp.com.mx:7085/sms/api/EnviarSMS";
         //OLD BUS
         // $sms_url = "http://10.200.3.102:7082/sms/api/EnviarSMS";
        }else{
